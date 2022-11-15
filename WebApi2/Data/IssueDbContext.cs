71ebd4c996047ab063b451a38aadcf7fb89f794c
ohmzth <63011212023@msu.ac.th>
2022-11-15 16:06:13 +0700

    Add project files.

---
+++ WebApi2/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿using WebApi2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi2.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext>options) : base (options)
+        {
+            
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
